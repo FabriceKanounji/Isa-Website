6854095733cbbbfaf61e9684e9fb23c0eaf15050
FabriceKanounji <94439672+FabriceKanounji@users.noreply.github.com>
2024-09-29 12:04:21 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -125,3 +125,30 @@ const slider = function () {
   });
 };
 slider();
+
+// Lange testimonial
+// Define the original and shortened texts
+var originalText = `"Het idee bestaat dat je naar een tot stand komende tekst moet ‘horen’. Het heeft iets te maken met geluid, met woordverloop, hoe iets ‘klinkt’ en hoe iets ‘moet’. Het idee bestaat dat schrijven veelal alleen-werk is. Je wil aan je eigen stem werken. Je wil ‘dat wat erin zit’ ‘eruit halen’. Schrijven is een vreemd labyrint; je wil het niet dwingen. Links en rechtsom dacht ik Isa Altink werk te sturen, opdat er iemand van buiten zou kunnen meeluisteren. Dat was éen van de meer verstandige keuzes die ik dit jaar heb gemaakt. Ik ben heel blij met haar leesintuïtie, begrip, vermogen om aan te wijzen wat er (nog) niet is, en dat vervolgens weer in begrijpbare taal terug te leggen."`;
+var shortenedText = `"Links en rechtsom dacht ik Isa Altink werk te sturen, opdat er iemand van buiten zou kunnen meeluisteren. Dat was éen van de meer verstandige keuzes die ik dit jaar heb gemaakt. Ik ben heel blij met haar leesintuïtie, begrip, vermogen om aan te wijzen wat er (nog) niet is, en dat vervolgens weer in begrijpbare taal terug te leggen."`;
+
+// Create a media query that matches screens with a max-width of 36em
+var mediaQuery = window.matchMedia("(max-width: 36em)");
+
+// Function to update the text based on the media query
+function updateTestimonialText(mediaQuery) {
+  var testimonialElement = document.querySelector(".testimonial-roelof");
+
+  if (mediaQuery.matches) {
+    // If the media query matches, change to the shortened text
+    testimonialElement.textContent = shortenedText;
+  } else {
+    // If the media query doesn't match, revert to the original text
+    testimonialElement.textContent = originalText;
+  }
+}
+
+// Initial check
+updateTestimonialText(mediaQuery);
+
+// Add a listener to detect changes
+mediaQuery.addListener(updateTestimonialText);
